f040f756a91eec129774e2e551e1584b9bf95282
JLifeOne <136247400+JLifeOne@users.noreply.github.com>
2025-10-19 22:46:23 -0500

    Remove obsolete React Native mock

--- packages/ui/test/mocks/react-native.ts
+++
@@ -1,7 +0,0 @@
-export const Pressable = (props: any) => props.children ?? null;
-export const Text = ({ children }: any) => children ?? null;
-export const View = ({ children }: any) => children ?? null;
-export const StyleSheet = {
-  create: (styles: any) => styles
-};
-export type ViewStyle = Record<string, unknown>;
